8e4d1c4c3ec857651e8a1f9e799bd42817f57aed
ramlittle <ramelitomartinez@gmail.com>
2025-04-29 14:55:25 +0800

    finished display for Logistic management

---
+++ js/administrative-module-pdfs.js
@@ -0,0 +1,29 @@
+const PDFContainer=document.querySelector('#administrative-module-pdfs-container');
+const PDFUrl = '../json/administrative-module-pdfs.json';
+
+function fetchData(){
+    fetch(PDFUrl)
+    .then(res=>res.json())
+    .then(data=>{
+       displayData(data);
+    })
+}
+
+function displayData(data){
+    if(!data.length){
+        return PDFContainer.insertAdjacentHTML('beforeend',`
+            <span>No Menu List Found</span>
+        `)
+    }
+    //display menu list here
+    for(let i=0;i<data.length;i++){
+        PDFContainer.insertAdjacentHTML('beforeend',`
+            <div class='pdf-container'>
+                <a href='${data[i].link}' target='_blank'>${data[i].title}</a>
+            </div>
+        `)
+    }
+}
+
+//Obtain Menu and Display
+fetchData();
