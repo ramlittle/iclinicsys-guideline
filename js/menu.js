bcce48185c93fbd87acabd85efa303d566ba78c4
ramlittle <ramelitomartinez@gmail.com>
2025-04-29 11:09:24 +0800

    placed other details

---
+++ js/menu.js
@@ -0,0 +1,31 @@
+const MenuListContainer=document.querySelector('.menu-list-container');
+const url = '../json/menu.json';
+
+function fetchMenu(){
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>{
+       displayMenu(data);
+    })
+}
+
+function displayMenu(data){
+    if(!data.length){
+        return MenuListContainer.insertAdjacentHTML('beforeend',`
+            <span>No Menu List Found</span>
+        `)
+    }
+    //display menu list here
+    for(let i=0;i<data.length;i++){
+        MenuListContainer.insertAdjacentHTML('beforeend',`
+            <div>
+                <a href=''>
+                    <img src='${data[i].image}'/>
+                <a>
+            </div>
+        `)
+    }
+}
+
+//Obtain Menu and Display
+fetchMenu();
